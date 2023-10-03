efeff73792e7de833c7da6fd83e39d6fe020e1ba
Aliaksandr Komar <Komarthebest1992@gmail.com>
2023-10-03 16:13:47 +0300

    ExampleTwoIO

--- IO_Java/ExampleTwo.java
+++ IO_Java/ExampleTwo.java
@@ -1,64 +1,39 @@
 package IO_Java;
 
 import java.io.PrintWriter;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class ExampleTwo {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        ArrayList<Integer> lengthLineArray = new ArrayList();
         int lengthConsoleLine;
         String consoleLine;
-        System.out.println("Please enter any string");
-
+        PrintWriter writer = new PrintWriter(System.out);
+        System.out.println("Please enter any string. To get the result, write 'result'");
         while (true) {
             consoleLine = scanner.nextLine();
-            if (consoleLine.equalsIgnoreCase("result")) {
-                break;
-            }
-
             lengthConsoleLine = consoleLine.length();
-
-            System.out.println("Output using with println. Length your string = " + lengthConsoleLine);
-            System.out.printf("Output using with printf. Length your string = %d", lengthConsoleLine);
-            try(PrintWriter pw = new PrintWriter(System.out))
-            {
-                pw.println("\nOutput using with printWriter" + lengthConsoleLine);
+            lengthLineArray.add(lengthConsoleLine);
+            if (consoleLine.equalsIgnoreCase("result")) {
+                scanner.close();
+                System.out.println("Output the length of your lines");
+                for (int i = 0; i < lengthLineArray.size() ; i++) {
+                    System.out.println("Output using with println. Length your string = " + lengthLineArray.get(i));
+                    System.out.printf("Output using with printf. Length your string = %d\n", lengthLineArray.get(i));
+                }
+                for (int line : lengthLineArray) {
+                    writer.println("Output using with printWriter. Length your string = " + line);
+                }
+                writer.flush();
             }
-
-
         }
-
-        }
-
-        /*Scanner scanner = new Scanner(System.in);
-
-        System.out.println("Введите несколько строк (для завершения введите 'exit'):");
-
-        while (true) {
-            String input = scanner.nextLine();
-
-            if (input.equalsIgnoreCase("exit")) {
-                break;
-            }
-
-            // Способ 1: Используя метод length() строки
-            int lengthMethod = input.length();
-            System.out.println("Длина строки (с использованием метода length()): " + lengthMethod);
-
-            // Способ 2: Считая символы в цикле
-            int lengthLoop = 0;
-            for (char c : input.toCharArray()) {
-                lengthLoop++;
-            }
-            System.out.println("Длина строки (с использованием цикла): " + lengthLoop);
-
-            // Способ 3: Используя метод codePointCount()
-            int lengthCodePoint = input.codePointCount(0, input.length());
-            System.out.println("Длина строки (с использованием метода codePointCount()): " + lengthCodePoint);
-        }
-
-        scanner.close();
-*/
+    }
 
     }
+
+
+
+
 
