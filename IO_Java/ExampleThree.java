8d9f685704929fdce206258ee3aa3518150f0610
Aliaksandr Komar <Komarthebest1992@gmail.com>
2023-10-08 13:35:43 +0300

    ExampleThreeIO

--- IO_Java/ExampleThree.java
+++ IO_Java/ExampleThree.java
@@ -4,30 +4,25 @@ import java.io.File;
 
 public class ExampleThree {
     public static void main(String[] args) {
-        // Замените путь на путь к вашей выбранной папке
-        String folderPath = "C:\\Users\\user\\Desktop\\OverOne\\ExampleThreeJavaIO";
-
-        File folder = new File(folderPath);
-
-        if (folder.exists() && folder.isDirectory()) {
-            File[] filesAndDirectories = folder.listFiles();
-
-            if (filesAndDirectories != null) {
-                for (File fileOrDir : filesAndDirectories) {
-                    String name = fileOrDir.getName();
-                    long length = fileOrDir.length();
-                    boolean isDirectory = fileOrDir.isDirectory();
-
-                    System.out.println("Название: " + name);
-                    System.out.println("Длина: " + length + " байт");
-                    System.out.println("Принадлежит к директории: " + isDirectory);
-                    System.out.println();
+        String thePathToTheFile = "C:\\Users\\user\\Desktop\\OverOne\\ExampleThreeJavaIO";
+        File dir = new File(thePathToTheFile);
+        if (dir.isDirectory() & dir.exists()) {
+            for (File item : dir.listFiles()) {
+                if (item.isDirectory()) {
+                    System.out.printf("\n%s - name of the folder inside the first folder. Folder size = %d байт.\n",
+                            item.getName(), item.length());
+                    System.out.println("Belongs to the directory: " + item.isDirectory() + "\n");
+                    for (File elementInsideItem : item.listFiles()) {
+                        System.out.printf("%s - name of the file inside the first folder. File size = %d байт.\n",
+                                elementInsideItem.getName(), elementInsideItem.length());
+                        System.out.println("Belongs to the directory: " + elementInsideItem.isDirectory() + "\n");
+                    }
+                } else {
+                    System.out.printf("%s - name of the file inside the first folder. File size = %d байт.\n",
+                            item.getName(), item.length());
+                    System.out.println("Belongs to the directory: " + item.isDirectory() + "\n");
                 }
-            } else {
-                System.out.println("Папка пуста.");
             }
-        } else {
-            System.out.println("Указанная папка не существует или не является директорией.");
         }
     }
 }
--- IO_Java/One.java
+++ IO_Java/One.java
@@ -1,30 +1,35 @@
 package IO_Java;
 
-
-
-import org.w3c.dom.ls.LSOutput;
-
 import java.io.File;
 
 public class One {
     public static void main(String[] args) {
-        String thePathToTheFile = "C:\\Users\\user\\Desktop\\OverOne\\ExampleThreeJavaIO";
-        File dir = new File(thePathToTheFile);
-        if (dir.isDirectory()) {
-            for (File item : dir.listFiles()){
-                if (item.isDirectory()) {
-                    System.out.printf("\n%s - name of the folder inside the first folder. Folder size = %d байт.\n",
-                            item.getName(), item.length());
-                    for (File elementInsideItem : item.listFiles()) {
-                        System.out.printf("%s - name of the file inside the first folder. File size = %d байт.\n",
-                                elementInsideItem.getName(), elementInsideItem.length());
-                    }
-                }else{
-                    System.out.printf("%s - name of the file inside the first folder. File size = %d байт.\n",
-                            item.getName(), item.length());
+        // Замените путь на путь к вашей выбранной папке
+        String folderPath = "C:\\Users\\user\\Desktop\\OverOne\\ExampleThreeJavaIO";
 
+        File folder = new File(folderPath);
+
+        if (folder.exists() && folder.isDirectory()) {
+            File[] filesAndDirectories = folder.listFiles();
+
+            if (filesAndDirectories != null) {
+                for (File fileOrDir : filesAndDirectories) {
+                    String name = fileOrDir.getName();
+                    long length = fileOrDir.length();
+                    boolean isDirectory = fileOrDir.isDirectory();
+
+                    System.out.println("Название: " + name);
+                    System.out.println("Длина: " + length + " байт");
+                    System.out.println("Принадлежит к директории: " + isDirectory);
+                    System.out.println();
                 }
+            } else {
+                System.out.println("Папка пуста.");
             }
+        } else {
+            System.out.println("Указанная папка не существует или не является директорией.");
         }
     }
-}
+        }
+
+
