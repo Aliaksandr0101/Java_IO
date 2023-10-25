d11d930847302a502f0b10c504b28bbfa15bf057
Aliaksandr Komar <Komarthebest1992@gmail.com>
2023-10-25 19:29:29 +0300

    ExampleFiveIO

---
+++ IO_Java/FileWriterReaderDemo.java
@@ -0,0 +1,26 @@
+package IO_Java;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class FileWriterReaderDemo {
+    public static void main(String[] args) {
+        char [] array = new char[50];
+        int size = 0;
+        File file = new File("fileWriter.txt");
+        try (FileWriter fw = new FileWriter(file);
+             FileReader fr = new FileReader(file)){
+            fw.write("Learn\nJava\n");
+            fw.flush();
+            size = fr.read(array);
+            System.out.println("Количество символов = " + size);
+            for (int i = 0; i < size ; i++) {
+                System.out.println("символ " + array[i]);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
