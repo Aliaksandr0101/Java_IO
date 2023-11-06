5737e717d244220ec63f7f7a5af869524ccde73f
Aliaksandr Komar <Komarthebest1992@gmail.com>
2023-11-06 13:11:20 +0300

    ExampleFiveIO

---
+++ IO_Java/PrintWriterDemoTwo.java
@@ -0,0 +1,24 @@
+package IO_Java;
+
+import org.w3c.dom.ls.LSOutput;
+
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+
+public class PrintWriterDemoTwo {
+    public static void main(String[] args) {
+        String s1 = "Привет мир!";
+        String s2 = "Hello world!";
+        try(PrintWriter printWriter  = new PrintWriter(
+                "C:\\Users\\user\\Desktop\\OverOne\\Java_IO\\BufferedReader\\FileWriterTwo.txt")){
+            printWriter.println(s1);
+            int i = 2;
+            printWriter.printf("Квадрат число %d равен %d %n", i, i*2);
+            printWriter.write(s2);
+            printWriter.print("Конец");
+            System.out.println("Запись в файл произведена");
+        } catch (FileNotFoundException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
