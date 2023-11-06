62c7919458447bde37f0330c3dcce66cbc7e4fda
Aliaksandr Komar <Komarthebest1992@gmail.com>
2023-11-06 21:30:42 +0300

    ExampleFiveIO

--- IO_Java/ExampleSix.java
+++ IO_Java/ExampleSix.java
@@ -1,4 +1,25 @@
 package IO_Java;
 
+import java.util.Scanner;
+
 public class ExampleSix {
-}
+    public static void main(String[] args) {
+            Scanner scanner = new Scanner(System.in);
+
+            System.out.print("Enter your name: ");
+            String name = scanner.nextLine();
+
+            System.out.print("Enter your surname: ");
+            String surname = scanner.nextLine();
+
+            System.out.print("Please enter your phone number: ");
+            String phoneNumber = scanner.nextLine();
+
+            scanner.close();
+            
+            System.out.printf(" User surname: %s%n User name: %s%n User phone number: %s%n", surname, name, phoneNumber);
+
+        }
+    }
+
+
